096693aa99cebf06927330e68c26fb925d0d7d00
Egor Ivin <ivinegor88@gmail.com>
2024-01-22 00:02:07 +0100

    026 add

---
+++ 029/task.js
@@ -0,0 +1,24 @@
+function howMuchILoveYou(nbPetals) {
+  switch (nbPetals) {
+    case (1, 7):
+      return "I love you";
+      break;
+    case 2:
+      return "a little";
+      break;
+    case 3:
+      return "a lot";
+      break;
+    case 4:
+      return "passionately";
+      break;
+    case 5:
+      return "madly";
+      break;
+    case 6:
+      return "not at all";
+      break;
+    default:
+      return "I love you";
+  }
+}
