3b65413c3b796494a1f9a296ac7908bfe902536e
renivi <ivinegor88@gmail.com>
2024-12-23 21:54:56 +0300

    codewars(8 kata): count-sheep

--- TS/count-sheep.ts
+++ TS/count-sheep.ts
@@ -1,9 +1,18 @@
+// First solution 
 export function countSheeps(arrayOfSheep: (boolean | undefined | null)[]) {
   let result = 0;
-  arrayOfSheep.map((el) => {
+  arrayOfSheep.forEach((el) => {
     if (el === true) {
       result += 1;
     }
   });
   return result;
+}
+
+// Better solution
+export function countSheeps2(arrayOfSheep: (boolean | undefined | null)[]) {
+  // your code here
+  return arrayOfSheep.reduce((acc, el) => {
+    return el ? acc + 1 : acc;
+  }, 0);
 }
