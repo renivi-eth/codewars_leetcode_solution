52581c0b410cd93e1a34705afb55135bc45022c7
renivi <ivinegor88@gmail.com>
2024-12-23 23:10:36 +0300

    task(frontend-task): object-to-array

---
+++ TS/object-to-array.ts
@@ -0,0 +1,26 @@
+// На вход приходит массив вида:
+const array = [
+  { name: 'width', value: 10 },
+  { name: 'height', value: 20 },
+];
+
+// Необходимо получить объект вида
+
+// {
+//   width: 10,
+//   height: 20,
+// }
+
+const result = array.reduce<Record<string, number>>((acc, item) => {
+  acc[item.name] = item.value;
+
+  return acc;
+}, {});
+
+// const obj: Record<string, any> = {}
+
+// const key = 'with'
+// const value = 10;
+
+// obj['with'] = 'bar'
+// obj[key] = 'bar'
