568ff9563aac0a959f6188299747c2e8ef80d7c8
renivi <ivinegor88@gmail.com>
2024-12-29 02:01:28 +0300

    feat(leetcode): filter-array

---
+++ TS/filter-array.ts
@@ -0,0 +1,24 @@
+type Fn = (n: number, i: number) => any;
+
+function filter(arr: number[], fn: Fn): number[] {
+  // Создание пустого массива для хранения отфильтрованных значений
+  const filtredArr = [];
+
+  // Цикл, который проходит по всем элементам массива
+  for (let i = 0; i < arr.length; i++) {
+    // Передаем в функцию каждый элемент массива и его индекс
+    if (fn(arr[i], i)) {
+      // Внутри блока if происходит вызов функции, которая возвращает true или false
+      // Например function more3(1) { return 1 > 3; } - false
+      filtredArr.push(arr[i]);
+    }
+  }
+
+  return filtredArr;
+}
+
+console.log(
+  filter([1, 2, 3, 4], function more3(n) {
+    return n > 3;
+  }),
+);
