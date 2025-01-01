57ebdd8d165beaf53f397f328a26569a6e635893
renivi <ivinegor88@gmail.com>
2025-01-01 23:22:40 +0300

    feat(leetcode): compose function

--- TS/function-compose.ts
+++ TS/function-compose.ts
@@ -5,3 +5,7 @@ function compose(functions: F[]): F {
     return functions.reduceRight((acc, fn) => fn(acc), x);
   };
 }
+
+const res = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x]);
+
+console.log(res(5)); // 5 * 2 = 10, 10 * 10 = 100, 100 + 1 = 101
